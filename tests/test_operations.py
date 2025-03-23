197307ef1ce81eec049ba2006170d56316b28e91
Anu Zagabathuni <anupamasajja@gmail.com>
2025-03-23 12:47:10 -0500

    workflow updates

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,4 +1,15 @@
 from src.math_operations import add, subtract
 
+def test_add():
+    assert add(1, 2) == 3
+    assert add(-1, 1) == 0
+    assert add(-1, -1) == -2
+
+
+def test_sub():
+    assert subtract(2, 1) == 1
+    assert subtract(1, 1) == 0
+    assert subtract(-1, -1) == 0
+
 
 
